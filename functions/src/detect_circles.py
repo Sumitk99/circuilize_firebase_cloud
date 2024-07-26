0556d8b7a799e5c2825a86b8e6bdecb698b566da
Sumit Kumar <54078335+Sumitk99@users.noreply.github.com>
2024-07-27 03:49:58 +0530

    Update detect_circles.py

--- functions/src/detect_circles.py
+++ functions/src/detect_circles.py
@@ -3,23 +3,18 @@ import numpy as np
 import sys
 import json
 
-# Read image.
 img = cv2.imread(sys.argv[1], cv2.IMREAD_COLOR)
 
-# Convert to grayscale.
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Blur using 3 * 3 kernel.
 gray_blurred = cv2.blur(gray, (3, 3))
 
-# Apply Hough transform on the blurred image.
 detected_circles = cv2.HoughCircles(gray_blurred,
                                     cv2.HOUGH_GRADIENT, 1, 20, param1=50,
                                     param2=43, minRadius=1, maxRadius=40)
 
 circle_count = 0
 
-# Draw circles that are detected.
 if detected_circles is not None:
     detected_circles = np.uint16(np.around(detected_circles))
     circle_count = len(detected_circles[0, :])
